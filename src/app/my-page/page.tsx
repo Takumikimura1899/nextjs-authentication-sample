9a4bb67cbf42752e4f3d7761a49279a0abcffef8
Takumikimura1899 <kimura141899@gmail.com>
2024-02-24 19:16:50 +0900

    redirect myPage

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,10 +1,12 @@
 import { auth } from '@/auth';
 import { SignOut } from '@/components/auth-components';
 import { LinkButton } from '@/components/ui/LinkButton';
+import { redirect } from 'next/navigation';
 
 export default async function MyPage() {
   const session = await auth();
-  const { name, email } = session?.user || { name: '', email: '' };
+  if (!session) redirect('/login');
+  const { name, email } = session.user;
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
       <h1 className='mb-8'>My Page</h1>
